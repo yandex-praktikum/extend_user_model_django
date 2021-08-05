353099c14e9a31625b14e846eb1bcc905cdda33a
Capwell <proninc@yandex.ru>
2021-08-05 14:53:12 +0400

    change signal

--- one_to_one/users/models.py
+++ one_to_one/users/models.py
@@ -19,11 +19,7 @@ class Profile(models.Model):
 
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
